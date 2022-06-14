b6e0c3702a419b41c43259a9e6827df1463db19d
Luis Miguel Bautista <lmiguelbautista@gmail.com>
2022-06-14 17:38:50 +0200

    Add FactoryBot gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,12 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "factory_bot_rails"
 
 class ActiveSupport::TestCase
+  # Setup FactoryBot
+  include FactoryBot::Syntax::Methods
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
