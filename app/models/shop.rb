b047645ef4b0dd4a4db1402344ddea9ad902a406
Luis Miguel Bautista <lmiguelbautista@gmail.com>
2022-06-30 00:13:29 +0200

    Map products within shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,6 +3,8 @@
 class Shop < ApplicationRecord
   include ShopifyApp::ShopSessionStorageWithScopes
 
+  has_many :products, class_name: "V1::Product", dependent: :destroy
+
   def api_version
     ShopifyApp.configuration.api_version
   end
