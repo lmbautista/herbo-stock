8eb96a725e5bef9693fa0a3b25904083612d3afd
Luis Miguel Bautista <lmiguelbautista@gmail.com>
2022-07-03 23:25:59 +0200

    Add pagination for audits

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
 class AuditsController < AuthenticatedController
+  DEFAULT_PAGE = 1
+  DEFAULT_PER_PAGE = 10
+
   def index
-    @audits = Audit.all.order(id: :desc).limit(10)
+    @audits = Audit.where(shop_domain: current_shopify_session.shop)
+      .order(id: :asc)
+      .page(page).per(per_page)
 
     render action: "index", layout: false
+  end
+
+  def per_page
+    params[:per_page].presence || DEFAULT_PER_PAGE
+  end
+
+  def page
+    params[:page].presence || DEFAULT_PAGE
   end
 end
--- test/controllers/audits_controller_test.rb
+++ test/controllers/audits_controller_test.rb
@@ -11,6 +11,38 @@ class AuditsControllerTest < ActionDispatch::IntegrationTest
 
       assert_response :ok
       assert_template :index
+      assert_includes @controller.view_assigns["audits"], audit
+    end
+  end
+
+  test "success filtering by shop" do
+    shop = create(:shop, shopify_domain: "test-2.shopify.shop")
+    audit_one = create(:audit)
+    audit_two = create(:audit, shop_domain: audit_one.shop.shopify_domain)
+    audit_three = create(:audit, shop_domain: shop.shopify_domain)
+
+    with_shopify_session(audit_one.shop) do
+      get audits_path, params: {}
+
+      assert_response :ok
+      assert_template :index
+      assert_includes @controller.view_assigns["audits"], audit_one
+      assert_includes @controller.view_assigns["audits"], audit_two
+      assert_not_includes @controller.view_assigns["audits"], audit_three
+    end
+  end
+
+  test "success with pagination" do
+    audit_one = create(:audit)
+    audit_two = create(:audit, shop_domain: audit_one.shop.shopify_domain)
+
+    with_shopify_session(audit_one.shop) do
+      get audits_path, params: { page: 1, per_page: 1 }
+
+      assert_response :ok
+      assert_template :index
+      assert_includes @controller.view_assigns["audits"], audit_one
+      assert_not_includes @controller.view_assigns["audits"], audit_two
     end
   end
 end
