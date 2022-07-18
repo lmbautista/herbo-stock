694e8a58af365df5075289546fc38ffec7508a06
Luis Miguel Bautista <lmiguelbautista@gmail.com>
2022-07-18 23:13:33 +0200

    Remove method no longer used

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,10 +10,4 @@ class HomeController < ApplicationController
     @host = params[:host]
     @products = V1::Product.all.map { [[_1.id, _1.definicion].join(" - "), _1.id] }
   end
-
-  private
-
-  def shop_session
-    ShopifyAPI::Utils::SessionUtils.load_offline_session(shop: @shop_origin)
-  end
 end
