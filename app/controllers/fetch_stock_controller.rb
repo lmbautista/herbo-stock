ff5f03ea36f987594bce7b3c162ba6aed14144a2
Luis Miguel Bautista <lmiguelbautista@gmail.com>
2022-07-19 00:11:02 +0200

    Refactor to use service to fetch stock

--- app/controllers/fetch_stock_controller.rb
+++ app/controllers/fetch_stock_controller.rb
@@ -2,29 +2,14 @@
 
 class FetchStockController < ApplicationController
   def show
-    render json: products, status: :ok
+    response = Shopify::FetchStock.new(create_params).call
+
+    render json: response.value, status: :ok
   end
 
   private
 
   def create_params
     params.permit(:shop, :sku).to_h
-  end
-
-  def sku
-    create_params[:sku]
-  end
-
-  def shop
-    @shop ||= Shop.find_by(shopify_domain: create_params[:shop])
-  end
-
-  def products
-    return {} if shop.blank?
-
-    products = V1::Product.where(shop: shop)
-    products = products.where(sku: sku) if sku.present?
-
-    products.pluck(:sku, :disponible).to_h
   end
 end
