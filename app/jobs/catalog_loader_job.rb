2f5d2db4f7c9ed9c4858f6bcef192425db700940
Luis Miguel Bautista <lmiguelbautista@gmail.com>
2022-07-28 07:26:45 +0200

    Refactor to allow repeat job on demand

--- app/jobs/catalog_loader_job.rb
+++ app/jobs/catalog_loader_job.rb
@@ -3,11 +3,67 @@
 class CatalogLoaderJob < ApplicationJob
   def perform(args)
     args.stringify_keys!
-    shop_domain = args.fetch("shop_domain")
-    product_ids = args.fetch("product_ids", [])
-    shop = ::Shop.find_by(shopify_domain: shop_domain)
+    @shop_domain = args.fetch("shop_domain")
+    @product_ids = args.fetch("product_ids", [])
+    @repeat = args.fetch("repeat", false)
+    @repeat_at = args.fetch("repeat_at", nil)
+    @process_id = args.fetch("process_id", nil)
 
+    program_next_repetition_if_needed
+      .and_then { schedule_repetition_if_needed }
+      .and_then { load_catalog }
+  end
+
+  private
+
+  attr_reader :shop_domain,
+              :product_ids,
+              :repeat,
+              :repeat_at,
+              :process_id,
+              :scheduler
+
+  def shop
+    @shop ||= ::Shop.find_by(shopify_domain: shop_domain)
+  end
+
+  def repeat?
+    repeat && CatalogLoaderScheduler.exists?(process_id: process_id)
+  end
+
+  def schedule_repetition_if_needed
+    return Response.success("Nothing to repeat") unless repeat?
+
+    args = {
+      repeat: true,
+      repeat_at: scheduler.next_scheduled_at,
+      process_id: process_id,
+      product_ids: product_ids,
+      shop_domain: shop.shopify_domain
+    }
+    CatalogLoaderJob.set(wait_until: repeat_at).perform_later(args)
+
+    Response.success("Repetition scheduled succesfully")
+  end
+
+  def program_next_repetition_if_needed
+    return Response.success("Nothing to repeat") unless repeat?
+
+    @scheduler = CatalogLoaderScheduler.find_by!(process_id: process_id)
+    return Response.success("Next repetition programmed") if scheduler.repeat
+
+    response_failure(scheduler)
+  end
+
+  def load_catalog
     Catalog::Loader.new(LOCAL_PATH, shop.id, product_ids).call
+  end
+
+  def response_failure(record)
+    id_error_message = "#{record.class}##{record.id}:"
+    error_message = [id_error_message, record.errors.full_messages.to_sentence].join(" ")
+
+    Response.failure(error_message)
   end
 
   # Temporal path that would be replaced by the provider dynamic URL
--- test/jobs/catalog_loader_job_test.rb
+++ test/jobs/catalog_loader_job_test.rb
@@ -22,4 +22,75 @@ class CatalogLoaderJobTest < ActiveJob::TestCase
 
     CatalogLoaderJob.new.perform(**job_params)
   end
+
+  test "success when repeat and exists available process_id" do
+    process_id = SecureRandom.hex(12)
+    repeat_at = Time.zone.now
+    next_scheduled_at = repeat_at + 1.minute
+    shop = create(:shop)
+    create(:catalog_loader_scheduler,
+           shop: shop, process_id: process_id, next_scheduled_at: repeat_at)
+    product_ids = [1003]
+    job_params = {
+      repeat: true,
+      repeat_at: repeat_at,
+      process_id: process_id,
+      product_ids: product_ids,
+      shop_domain: shop.shopify_domain
+    }
+
+    loader_response = :ok
+    loader_mock = mock
+    loader_mock.expects(:call).returns(Response.success(loader_response))
+    CatalogLoaderScheduler.any_instance.stubs(:next_scheduled_at).returns(next_scheduled_at)
+
+    Catalog::Loader
+      .expects(:new)
+      .with(CatalogLoaderJob::LOCAL_PATH, shop.id, product_ids)
+      .returns(loader_mock)
+
+    assert_enqueued_with job: ::CatalogLoaderJob,
+                         args: [job_params.merge(repeat_at: next_scheduled_at)],
+                         queue: "default" do
+      response = CatalogLoaderJob.new.perform(**job_params)
+
+      assert response.success?
+      assert_equal loader_response, response.value
+    end
+  end
+
+  test "success when repeat and does not exist available process_id" do
+    process_id = SecureRandom.hex(12)
+    new_process_id = SecureRandom.hex(12)
+    repeat_at = Time.zone.now
+    next_scheduled_at = repeat_at + 1.minute
+    shop = create(:shop)
+    create(:catalog_loader_scheduler,
+           shop: shop, process_id: new_process_id, next_scheduled_at: repeat_at)
+    product_ids = [1003]
+    job_params = {
+      repeat: true,
+      repeat_at: repeat_at,
+      process_id: process_id,
+      product_ids: product_ids,
+      shop_domain: shop.shopify_domain
+    }
+
+    loader_response = :ok
+    loader_mock = mock
+    loader_mock.expects(:call).returns(Response.success(loader_response))
+    CatalogLoaderScheduler.any_instance.stubs(:next_scheduled_at).returns(next_scheduled_at)
+
+    Catalog::Loader
+      .expects(:new)
+      .with(CatalogLoaderJob::LOCAL_PATH, shop.id, product_ids)
+      .returns(loader_mock)
+
+    assert_no_enqueued_jobs only: ::CatalogLoaderJob do
+      response = CatalogLoaderJob.new.perform(**job_params)
+
+      assert response.success?
+      assert_equal loader_response, response.value
+    end
+  end
 end
