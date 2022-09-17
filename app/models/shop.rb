a8e7dd6a510281d9cf43e9f210099a8738f4c74a
Luis Miguel Bautista <lmiguelbautista@gmail.com>
2022-09-17 16:30:45 +0200

    Add relation to destroy in cascade

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,6 +4,7 @@ class Shop < ApplicationRecord
   include ShopifyApp::ShopSessionStorageWithScopes
 
   has_many :products, class_name: "V1::Product", dependent: :destroy
+  has_one :catalog_loader_scheduler, dependent: :destroy
 
   def api_version
     ShopifyApp.configuration.api_version
